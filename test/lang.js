d4784ebc9fa36d337ba6800644e480641444a0da
Feelfree <rj23@protonmail.com>
2016-12-03 16:40:12 +0100

    Update lang.js

--- test/lang.js
+++ test/lang.js
@@ -7,7 +7,6 @@
 
 var meta = require('../index');
 var assert = require('./utils/assert.js');
-var request = require('request');
 
 // mocha defines to avoid JSHint breakage
 /* global describe, it, before, beforeEach, after, afterEach */
@@ -19,11 +18,10 @@ describe('scraping', function() {
 	var url;
 
 	describe('Lang parameter from external website', function() {
-		it('should resolve promise from woorank', function() {
+		it('should get hu parameter', function() {
 			var expected = "hu";
 			var options =  {
     		url: "http://mno.hu/migr_1834/tellerlevel-cafolat-es-cafolat-700280",
-    		jar: request.jar(), // Cookie jar
     		headers: {
         	'User-Agent': 'webscraper'
     		}
