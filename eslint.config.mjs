9a6f8b2531b26c9f123611d8a92a30de821be25b
Jacobojijo <jacobojijo2017@gmail.com>
2024-07-18 13:03:52 +0300

    remove eslint files

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
