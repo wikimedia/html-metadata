f743b468f741c26cebddff568f6d9339ce6889b9
Marielle Volz <marielle.volz@gmail.com>
2025-03-04 14:01:27 +0000

    build: update microdata-node and add tests (#118)
    * build: update microdata-node
    
    * Add schema.org static tests
    
    * Add schema.org microdata to turtle_article.html
    and case sensitive tests.
     ** Includes self-closing tags, href attr, content attr,
    and content taken from between tags
    * Add test to ensure self-closing tags are parsed
    correctly.

--- test/static.js
+++ test/static.js
@@ -11,40 +11,61 @@ const meta = require( '../index' );
 // mocha defines to avoid eslint breakage
 /* global describe, it */
 
-describe( 'static files', () => {
+describe( 'static tests', () => {
 	let $;
 	const fs = require( 'fs' );
 	let expected;
 
-	it( 'should get correct info from turtle movie file', () => {
-		expected = JSON.parse( fs.readFileSync( './test/static/turtle_movie.json' ) );
-		$ = cheerio.load( fs.readFileSync( './test/static/turtle_movie.html' ) );
-		return meta.parseAll( $ ).then( ( results ) => {
-			assert.deepEqual( results, expected );
+	describe( 'static files', () => {
+		it( 'should get correct info from turtle movie file', () => {
+			expected = JSON.parse( fs.readFileSync( './test/static/turtle_movie.json' ) );
+			$ = cheerio.load( fs.readFileSync( './test/static/turtle_movie.html' ) );
+			return meta.parseAll( $ ).then( ( results ) => {
+				assert.deepEqual( results, expected );
+			} );
+		} );
+
+		it( 'should get correct info from turtle article file', () => {
+			expected = JSON.parse( fs.readFileSync( './test/static/turtle_article.json' ) );
+			$ = cheerio.load( fs.readFileSync( './test/static/turtle_article.html' ) );
+			return meta.parseAll( $ ).then( ( results ) => {
+				assert.deepEqual( results, expected );
+			} );
+		} );
+
+		it( 'should be case insensitive on turtle article file', () => {
+			expected = JSON.parse( fs.readFileSync( './test/static/turtle_article.json' ) );
+			$ = cheerio.load( fs.readFileSync( './test/static/turtle_article_case.html' ) );
+			return meta.parseAll( $ ).then( ( results ) => {
+				assert.deepEqual( results, expected );
+			} );
+		} );
+
+		it( 'should be case insensitive on turtle article file', () => {
+			expected = JSON.parse( fs.readFileSync( './test/static/turtle_article.json' ) );
+			$ = cheerio.load( fs.readFileSync( './test/static/turtle_article_case.html' ) );
+			return meta.parseAll( $ ).then( ( results ) => {
+				assert.deepEqual( results, expected );
+			} );
 		} );
 	} );
 
-	it( 'should get correct info using loadFromString method from turtle movie file ', () => {
-		expected = JSON.parse( fs.readFileSync( './test/static/turtle_movie.json' ) );
-		const html = fs.readFileSync( './test/static/turtle_movie.html' );
-		return meta.loadFromString( html ).then( ( results ) => {
-			assert.deepEqual( results, expected );
+	describe( 'loadFromString', () => {
+		it( 'should get correct info using loadFromString method from turtle movie file ', () => {
+			expected = JSON.parse( fs.readFileSync( './test/static/turtle_movie.json' ) );
+			const html = fs.readFileSync( './test/static/turtle_movie.html' );
+			return meta.loadFromString( html ).then( ( results ) => {
+				assert.deepEqual( results, expected );
+			} );
+		} );
+
+		it( 'should get correct info using loadFromString method for self closing tag', () => {
+			const html = '<div itemscope><span itemprop="price" content="139.90" /> <span itemprop="priceCurrency" content="PLN" /></div>';
+			const expected = { schemaOrg: { items: [ { properties: { priceCurrency: [ 'PLN' ], price: [ '139.90' ] } } ] } };
+			return meta.loadFromString( html ).then( ( results ) => {
+				assert.deepEqual( results, expected );
+			} );
 		} );
 	} );
 
-	it( 'should get correct info from turtle article file', () => {
-		expected = JSON.parse( fs.readFileSync( './test/static/turtle_article.json' ) );
-		$ = cheerio.load( fs.readFileSync( './test/static/turtle_article.html' ) );
-		return meta.parseAll( $ ).then( ( results ) => {
-			assert.deepEqual( results, expected );
-		} );
-	} );
-
-	it( 'should be case insensitive on turtle article file', () => {
-		expected = JSON.parse( fs.readFileSync( './test/static/turtle_article.json' ) );
-		$ = cheerio.load( fs.readFileSync( './test/static/turtle_article_case.html' ) );
-		return meta.parseAll( $ ).then( ( results ) => {
-			assert.deepEqual( results, expected );
-		} );
-	} );
 } );
